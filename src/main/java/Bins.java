41a3743b0c66e3e2624e21323ec5ba40c68313a8
Brandon Chambers <brandonmchambers36@gmail.com>
2019-10-30 20:04:57 -0400

    begin debugging

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,12 @@
 
 public class Bins {
+    //Private variables, are variables that are visible only to the class to which they belong.
+    private Integer numberOfDie;
+    private Integer numberOfTosses;
+
+    public Bins(Integer numberOfDie, Integer numberOfTosses) {
+        this.numberOfDie = numberOfDie;
+        this.numberOfTosses = numberOfTosses;
+    }
 
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,8 @@
 public class Simulation {
 
-
+    public static void main(String[] args) {
+        Dice rollTest = new Dice(0);
+        rollTest.rollDice(4);
+    }
 
 }
